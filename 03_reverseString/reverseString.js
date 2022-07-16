e7c8590318e31b411f85143a96bed800f4248fd3
Matt Shrode <mattshrode@gmail.com>
2022-07-15 22:48:25 -0500

    Complete Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    const phraseArray = Array.from(phrase);
+    let reverseArray = phraseArray.reverse();
+    let newPhrase = reverseArray.join("");
+    return newPhrase;
 };
 
 // Do not edit below this line
